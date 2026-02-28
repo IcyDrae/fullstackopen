d7f319ce2f88f26d55ffaa06ba3bb10f85301447
IcyDrae <35934418+IcyDrae@users.noreply.github.com>
2026-02-28 15:17:55 +0100

    part1: unicafe step 1

--- part1/UniCafe/src/App.jsx
+++ part1/UniCafe/src/App.jsx
@@ -5,9 +5,29 @@ function App() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  function setGoodHandler() {
+    setGood(good + 1);
+  }
+
+  function setNeutralHandler() {
+    setNeutral(neutral + 1);
+  }
+
+  function setBadHandler() {
+    setBad(bad + 1);
+  }
+
   return (
     <>
-      Hello World
+      <h1>give feedback</h1>
+      <button onClick={setGoodHandler}>good</button>
+      <button onClick={setNeutralHandler}>neutral</button>
+      <button onClick={setBadHandler}>bad</button>
+
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </>
   )
 }
