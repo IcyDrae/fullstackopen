2d1ca0abd836751afe23cfd53b35a392ad6d35b8
IcyDrae <35934418+IcyDrae@users.noreply.github.com>
2026-03-03 14:39:59 +0100

    part2: The Phonebook Step 4

--- part2/PhoneBook/src/App.jsx
+++ part2/PhoneBook/src/App.jsx
@@ -4,10 +4,17 @@ function App() {
   const [persons, setPersons] = useState([
     {
       id: 1,
-      name: 'Arto Hellas'
+      name: 'Arto Hellas',
+      phone: ""
     }
-  ]) 
+  ]);
+
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -24,6 +31,7 @@ function App() {
     const noteObject = {
       id: persons.length + 1,
       name: newName,
+      phone: newNumber
     }
 
     setPersons(persons.concat(noteObject))
@@ -34,22 +42,39 @@ function App() {
     setNewName(event.target.value);
   }
 
+  const [result, setResult] = useState("");
+
+  const handleResult = (event) => {
+    setResult(event.target.value);
+  };
+
+  const personsToShow = persons.filter(person => {
+    return person.name.toLowerCase().includes(result.toLowerCase());
+  });
+
   return (
     <>
       <div>
       <h2>Phonebook</h2>
+      filter shown with: <input value={result} onChange={handleResult}/>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handlePersonChange}/>
+          <br />
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      { persons.map((person) => {
-        return <p key={person.id}>{person.name}</p>
-      }) }
+      {personsToShow.map((person) => {
+        return (
+          <div key={person.id}>
+            {person.name} {person.phone}
+          </div>
+        )
+      })}
     </div>
     </>
   )
