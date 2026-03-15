7004d08238326f1a38683c25cd259c42015c3491
IcyDrae <35934418+IcyDrae@users.noreply.github.com>
2026-03-15 15:32:50 +0100

    part4: Helper Functions and Unit Tests, step 2

--- part4/BlogList/tests/list_helper.test.js
+++ part4/BlogList/tests/list_helper.test.js
@@ -1,13 +1,41 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
+describe('total likes', () => {
 
-  const result = listHelper.dummy(blogs)
+  const emptyList = []
 
-  assert.strictEqual(result, 1)
-})
+  const listWithOneBlog = [
+    {
+      _id: '1',
+      title: 'Test blog',
+      author: 'Reard',
+      url: 'http://example.com',
+      likes: 5
+    }
+  ]
+
+  const listWithManyBlogs = [
+    { title: 'a', likes: 5 },
+    { title: 'b', likes: 3 },
+    { title: 'c', likes: 7 }
+  ]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes(emptyList)
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of many blogs is calculated correctly', () => {
+    const result = listHelper.totalLikes(listWithManyBlogs)
+    assert.strictEqual(result, 15)
+  })
+});
 
